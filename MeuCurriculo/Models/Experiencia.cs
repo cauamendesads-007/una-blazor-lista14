f7509863ebe687ca2852741f9f443b82ec7c2c81
cauamendesads-007 <cauamendesads@gmail.com>
2026-04-24 21:00:41 -0300

    Add files via upload

---
+++ MeuCurriculo/Models/Experiencia.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace MeuCurriculo.Models
+{
+    public class Experiencia
+    {
+        public String Empresa { get; set; }
+        public String Cargo { get; set; }
+        public String Inicio { get; set; } = "Atual";
+        public String Fim { get; set; }
+        public String Descricao { get; set; }
+        
+    }
+}
